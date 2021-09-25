f073ff79b31700460a07ec4479dbc97da39694c3
rpgarde <rpgarde@gmail.com>
2021-09-25 20:05:35 +1000

    Basic CRUD functionality done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground.js')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     // useNewUrlParser:true,
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',{
 })
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
@@ -48,6 +50,24 @@ app.get('/campgrounds/:id',async (req,res) => {
     res.render('campgrounds/show', {campground})
 })
 
+// Render form to edit
+app.get('/campgrounds/:id/edit',async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground})
+})
+
+// Edit the campground
+app.put('/campgrounds/:id',async (req,res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// delete campground
+app.delete('/campgrounds/:id',async (req,res) => {
+    const campground = await Campground.findByIdAndDelete(req.params.id);
+    console.log('Successfuly deleted')
+    res.redirect(`/campgrounds`)
+})
 
 app.get('/makecampground',async (req,res)=>{
     const camp = new Campground({title:'My Backyard'})
